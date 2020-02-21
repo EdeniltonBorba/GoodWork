f4db291182dc84b350f69cd9e36fdf96da2b0136
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-21 13:18:44 +0100

    add nav

--- goodwork/src/components/Navbar.js
+++ goodwork/src/components/Navbar.js
@@ -2,9 +2,18 @@ import React from "react";
 
 function Nav() {
     return (
-        <nav>
 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand">
+                GoodWork
+                </a>
+            <form className="form-inline">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </nav>
+
+
     );
 
 }
