4e0f15ca1c92bc8172c83aa89bc1c08b8678fc16
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-22 14:41:01 +0100

    add screen SignIn

--- goodwork/src/pages/SignIn/SignIn.js
+++ goodwork/src/pages/SignIn/SignIn.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import { Link } from "react-router-dom";
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -13,6 +15,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Logo from '../../components/Logo/index.js';
+
 
 
 function Copyright() {
@@ -102,7 +105,7 @@ export default function SignIn() {
               </Link>
                         </Grid>
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link to="/" variant="body2">
                                 {"Don't have an account? Sign Up"}
                             </Link>
                         </Grid>
--- goodwork/src/pages/Signup/signup.js
+++ goodwork/src/pages/Signup/signup.js
@@ -1,24 +1,27 @@
 import React from 'react';
+
+import { Link } from "react-router-dom";
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Logo from '../../components/Logo/index.js'
+import Logo from '../../components/Logo/index.js';
+import SignIn from '../../pages/SignIn/SignIn.js';
 
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
+            <Link to="/" href="https://material-ui.com/">
                 Your Website
       </Link>{' '}
             {new Date().getFullYear()}
@@ -124,7 +127,7 @@ export default function SignUp() {
           </Button>
                     <Grid container justify="flex-end">
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link to="/SignIn" variant="body2">
                                 Already have an account? Sign in
               </Link>
                         </Grid>
