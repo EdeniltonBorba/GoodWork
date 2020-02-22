e8967b51371c52abc9664f8b8c638fb0a4c0dcea
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-22 12:39:27 +0100

    add screen home

--- goodwork/src/App.js
+++ goodwork/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Nav from "./components/Navbar/index.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+import Signup from "./pages/Signup/signup.js";
+
 
 function App() {
   return (
-    <div>
-      <Nav />
-
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Signup} />
+        <Route exact path="/Home" component={Home} />
+      </div>
+    </Router>
   );
 }
 
---
+++ goodwork/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../../components/Navbar/index.js";
+
+function Home() {
+    return (
+        <Navbar />
+    )
+}
+
+export default Home;
+
