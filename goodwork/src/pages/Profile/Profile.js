8d8dd1aeb1d7a5f1d8be2afec7e3184ea5301b1f
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-23 20:13:34 +0100

    add profile

--- goodwork/src/pages/Profile/Profile.js
+++ goodwork/src/pages/Profile/Profile.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Navbar from "../../components/Navbar/index.js";
+import Container from '../../components/Container/index.js';
+import CardContainer from '../../components/CardContainer/index.js';
 
 function Profile() {
     return (
-        <Navbar />
+        <div>
+            <Navbar />
+            <Container>
+                <CardContainer />
+            </Container>
+        </div>
+
+
+
+
     )
 }
 
