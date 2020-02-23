f184403a83741d49862dbb506bb2758e87742c22
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-23 20:43:36 +0100

    add profile

--- goodwork/src/pages/Home/Home.js
+++ goodwork/src/pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Navbar from "../../components/Navbar/index.js";
-import { Container } from "@material-ui/core";
 import CardContainer from "../../components/CardContainer/index.js";
 import CardImage from "../../components/CardImage/index.js";
+import Container from "../../components/Container/index.js"
+
 
 
 function Home() {
