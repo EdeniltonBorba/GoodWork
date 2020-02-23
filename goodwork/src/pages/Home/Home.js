675f43cda7d9e1003836f782a1bbf5607b412a65
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-23 17:55:46 +0100

    add cardImag

--- goodwork/src/pages/Home/Home.js
+++ goodwork/src/pages/Home/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Navbar from "../../components/Navbar/index.js";
+import { Container } from "@material-ui/core";
+import CardContainer from "../../components/CardContainer/index.js";
+import CardImage from "../../components/CardImage/index.js";
 
 function Home() {
     return (
-        <Navbar />
+        <div>
+            <Navbar />
+            <Container>
+                <CardContainer />
+                <CardImage />
+
+
+            </Container>
+        </div>
     )
 }
 
