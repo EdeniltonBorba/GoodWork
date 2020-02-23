5e031e46d17a3cb9d3d6cb2a6e86fd282b1db19d
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-23 12:10:23 +0100

    add appBar

--- goodwork/src/components/Navbar/index.js
+++ goodwork/src/components/Navbar/index.js
@@ -14,6 +14,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import Logo from '../Logo';
 
 const useStyles = makeStyles(theme => ({
     grow: {
@@ -60,7 +61,7 @@ const useStyles = makeStyles(theme => ({
         transition: theme.transitions.create('width'),
         width: '100%',
         [theme.breakpoints.up('md')]: {
-            width: 200,
+            width: 500,
         },
     },
     sectionDesktop: {
@@ -163,17 +164,10 @@ export default function PrimarySearchAppBar() {
         <div className={classes.grow}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="open drawer"
-                    >
-                        <MenuIcon />
-                    </IconButton>
+
                     <Typography className={classes.title} variant="h6" noWrap>
-                        Material-UI
-          </Typography>
+                        <Logo />
+                    </Typography>
                     <div className={classes.search}>
                         <div className={classes.searchIcon}>
                             <SearchIcon />
