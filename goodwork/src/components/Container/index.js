9737cf721e77109d6893f87d774dbae76e989e6c
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-23 17:35:31 +0100

    add card

--- goodwork/src/components/Container/index.js
+++ goodwork/src/components/Container/index.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
-function Container(props) {
-    return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
+export default function SimpleContainer() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+            </Container>
+        </React.Fragment>
+    );
 }
-
-export default Container;
