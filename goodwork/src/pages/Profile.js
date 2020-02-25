044c59dc7bfaa15467ba5679e63e2398d80aa1dc
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-25 09:49:26 +0100

    profile

--- goodwork/src/pages/Profile.js
+++ goodwork/src/pages/Profile.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import { Container, Row, Col } from "../components/Grid/index.js";
 
 function Profile() {
     return (
 
         <div>
             <Navbar />
+            <Container fluid>
+                <Row>
+                    <Col size="md-6 sm-12">
+
+                    </Col>
+                </Row>
+            </Container>
 
         </div>
 
