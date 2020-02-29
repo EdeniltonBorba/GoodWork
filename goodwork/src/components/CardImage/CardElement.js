2c67135090c87bc0a29bc213681cab43f2fff119
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-29 11:21:09 +0100

    global state

---
+++ goodwork/src/components/CardImage/CardElement.js
@@ -0,0 +1,117 @@
+
+import React from "react"
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import CallIcon from '@material-ui/icons/Call';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        maxWidth: 500,
+        margin: 10,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
+
+export default ({ profi }) => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const [likes, addLikes] = React.useState(0)
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return <Card className={classes.root}>
+        <CardHeader
+            avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+
+                </Avatar>
+            }
+            action={
+                <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                </IconButton>
+            }
+            title={profi.name}
+
+        />
+        <CardMedia
+            className={classes.media}
+            image={profi.img}
+
+        />
+        <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {profi.hireMe}
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites" onClick={() => addLikes(likes + 1)}>
+                <FavoriteIcon />
+            </IconButton>
+            Likes: {likes}
+            <IconButton
+                className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+            >
+                <ExpandMoreIcon />
+            </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+                <Typography paragraph>Contact:</Typography>
+                <Typography paragraph>
+                    <MailOutlineIcon>
+                        {profi.email}
+                    </MailOutlineIcon>
+                </Typography>
+                <Typography paragraph>
+                    <CallIcon fone={profi.fone} />
+                </Typography>
+                <Typography paragraph>Comment:</Typography>
+                <Typography paragraph>
+                    {profi.reviews}
+                </Typography>
+
+            </CardContent>
+        </Collapse>
+    </Card>
+}
--- goodwork/src/components/CardImage/CardImgProfile.js
+++ goodwork/src/components/CardImage/CardImgProfile.js
@@ -1,120 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import CallIcon from '@material-ui/icons/Call';
+import CardElement from "./CardElement"
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        maxWidth: 500,
-        margin: 10,
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    avatar: {
-        backgroundColor: red[500],
-    },
-}));
+
 
 export default function RecipeReviewCard({ profis }) {
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
 
     return (
         <React.Fragment>
             {profis.length ? profis.map(profi => {
-                return <Card className={classes.root}>
-                    <CardHeader
-                        avatar={
-                            <Avatar aria-label="recipe" className={classes.avatar}>
-                                R
-  </Avatar>
-                        }
-                        action={
-                            <IconButton aria-label="settings">
-                                <MoreVertIcon />
-                            </IconButton>
-                        }
-                        title={profi.name}
-
-                    />
-                    <CardMedia
-                        className={classes.media}
-                        image={profi.img}
-
-                    />
-                    <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {profi.hireMe}
-                        </Typography>
-                    </CardContent>
-                    <CardActions disableSpacing>
-                        <IconButton aria-label="add to favorites">
-                            <FavoriteIcon />
-                        </IconButton>
-                        <IconButton aria-label="share">
-                            <ShareIcon />
-                        </IconButton>
-                        <IconButton
-                            className={clsx(classes.expand, {
-                                [classes.expandOpen]: expanded,
-                            })}
-                            onClick={handleExpandClick}
-                            aria-expanded={expanded}
-                            aria-label="show more"
-                        >
-                            <ExpandMoreIcon />
-                        </IconButton>
-                    </CardActions>
-                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <CardContent>
-                            <Typography paragraph>Contact:</Typography>
-                            <Typography paragraph>
-                                <MailOutlineIcon>
-                                    {profi.email}
-                                </MailOutlineIcon>
-                            </Typography>
-                            <Typography paragraph>
-                                <CallIcon fone={profi.fone} />
-                            </Typography>
-                            <Typography paragraph>Comment:</Typography>
-                            <Typography paragraph>
-                                {profi.reviews}
-                            </Typography>
-
-                        </CardContent>
-                    </Collapse>
-                </Card>
+                return <CardElement profi={profi} />
             }) : "Please select a category"}
         </React.Fragment>
 
