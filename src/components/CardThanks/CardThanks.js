7293876eaee2cb57850c0ceaf06fe4502977c5a0
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-03 20:55:46 +0100

    add thanks

---
+++ src/components/CardThanks/CardThanks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "../Grid/index.js";
+
+export function CardThanks() {
+
+    const styleCard = {
+        backgroundImage: "",
+    }
+
+    return (
+
+        <div>
+            <Container>
+
+            </Container>
+
+        </div>
+
+    )
+}
+
+export default CardThanks;
