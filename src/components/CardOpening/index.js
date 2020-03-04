17fe5ddbadb01a6b89b175575865916159b81b67
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-04 11:12:31 +0100

    add page opening

--- src/components/CardOpening/index.js
+++ src/components/CardOpening/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Background from './family.jpg';
 import "./img.css"
+import { Link } from "react-router-dom";
 
 export default () => <div className="familyRoot">
     <img className="familyImage" src={Background} />
-    <p className="familyText"> Thanks!</p>
+    <div className="familyText"> Thanks!<Link to="/SignIn"><button className="buttonSignIn">SIGN IN</button></Link><button className="buttonSignUp">SIGN UP</button></div>
 </div>
 
--- src/components/CardThanks/index.js
+++ src/components/CardThanks/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Background from './family.jpg';
 import "./img.css"
 
+
 export default () => <div className="familyRoot">
     <img className="familyImage" src={Background} />
     <p className="familyText"> Thanks!</p>
