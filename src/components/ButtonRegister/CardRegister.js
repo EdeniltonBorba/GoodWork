0102c2bd9c73b775f1f28e8190b1883a658ec0e1
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-04 11:17:55 +0100

    add cardRegister

---
+++ src/components/ButtonRegister/CardRegister.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import UserProfile from "../CardProfile/UserProfile.js";
+import ButtonRegister from '../ButtonRegister/ButtonRegister.js';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            marginLeft: theme.spacing(40),
+            marginTop: theme.spacing(2),
+            width: theme.spacing(100),
+            height: theme.spacing(80),
+        },
+    },
+}));
+
+export default function SimplePaper({ profiName }) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Paper elevation={3}>
+                <UserProfile />
+
+                <ButtonRegister />
+            </Paper>
+        </div>
+    );
+}
