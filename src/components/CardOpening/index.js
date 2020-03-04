ddbd3c11a16bdecbbf4070093a3af4d04f78074b
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-04 15:21:49 +0100

    add cardRegister

--- src/components/CardOpening/index.js
+++ src/components/CardOpening/index.js
@@ -5,8 +5,9 @@ import { Link } from "react-router-dom";
 
 export default () => <div className="familyRoot">
     <img className="familyImage" src={Background} />
-    <div className="familyText"><p>Help around the home, at your fingertips</p>
-        <p>You don't always have to DIY. Get help from thousands of trusted Taskers for everything from home repairs to cleaning.</p>
-        <Link to="/SignIn"><button className="buttonSignIn">SIGN IN</button></Link><button className="buttonSignUp">SIGN UP</button></div>
+    <div className="familyText">Help around the home, at your fingertips</div>
+    <div>You don't always have to DIY. Get help from thousands of trusted Taskers for everything from home repairs to cleaning.</div>
+    <Link to="/SignIn"><button className="buttonSignIn">SIGN IN</button></Link><button className="buttonSignUp">SIGN UP</button>
 </div>
+
 
