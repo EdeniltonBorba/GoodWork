bc9321dd038d47c1c25e3fb52c5ced33ae957508
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-04 17:26:12 +0100

    add page opening

--- src/components/CardOpening/index.js
+++ src/components/CardOpening/index.js
@@ -6,8 +6,12 @@ import { Link } from "react-router-dom";
 export default () => <div className="familyRoot">
     <img className="familyImage" src={Background} />
     <div className="familyText">Help around the home, at your fingertips</div>
-    <div className="familyTextSign">You don't always have to DIY. Get help from thousands of trusted Taskers for everything from home repairs to cleaning.</div>
-    <Link to="/SignIn"><button className="buttonSignIn">SIGN IN</button></Link><button className="buttonSignUp">SIGN UP</button>
+    <div className="familyTextSign">
+        You don't always have to DIY. Get help from thousands of trusted Taskers for everything from home repairs to cleaning.
+        <br />
+        <Link to="/SignIn"><button className="buttonSign">Sign In</button></Link><Link to="/SignUp"> <button className="buttonSign">Sign Up</button></Link>
+    </div>
 </div>
+
 
 
