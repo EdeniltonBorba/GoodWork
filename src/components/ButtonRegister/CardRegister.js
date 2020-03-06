3c0f570082c51093b50ae9ed25b97166551d6cce
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-06 21:25:35 +0100

    update profile

--- src/components/ButtonRegister/CardRegister.js
+++ src/components/ButtonRegister/CardRegister.js
@@ -1,51 +1,25 @@
 import React from "react";
-import Background from './family.jpg';
+import Background from './register.jpg';
 import "./img.css"
 import { Link } from "react-router-dom";
-
+import ButtonRegister from "./ButtonRegister.js";
 
 export default () => <div>
     <div className="homeRoot">
-        <div className="homeImage" />
+        <div className="RegisterImage" />
         <div className="homeBox">
-            <div className="homeText"><span class="customHelp">Help</span> around the home, at your fingertips</div>
-        </div>
-    </div>
-    <div className="homeSubtitle">
-        <div className="mainTitle">
-            You don't always have to DIY. Get help from thousands of trusted Taskers for everything from home repairs to cleaning.
+            <div className="mainTitle">
+                <div className="TextOne">
+                    Start Tasking.
+            </div>
+                <div className="TextTwo">
 
-        </div>
-        <div className="homeCtas">
-            <Link to="/SignIn"><button className="buttonSign">Sign In</button></Link><Link to="/SignUp"> <button className="buttonSign">Sign Up</button></Link>
+                </div>
+                <div className="Textthree">
 
+                </div>
+            </div>
         </div>
 
     </div>
 </div>
-/*
-
-import React from "react";
-import Background from './family.jpg';
-import "./img.css"
-import ButtonRegister from '../ButtonRegister/ButtonRegister.js';
-
-
-
-
-export default () => <div className="familyRoot">
-    <img className="familyImage" src={Background} />
-    <div className="familyTextOne">
-        <h1>Start Tasking.</h1>
-    </div>
-    <div className="familyTextTwo">
-        <h2>Earn money your way.</h2>
-    </div>
-    <div className="familyTexThree">
-        <h3>Be someone's hero today. Earn money by helping people with their everyday to-dos.</h3>
-    </div>
-    <div className="btn">
-        <ButtonRegister />
-    </div>
-</div >
-*/
