daca55be5e74884ea7e566acc57808c0dc717bf6
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-07 00:15:18 +0100

    update profile

--- src/components/ButtonRegister/CardRegister.js
+++ src/components/ButtonRegister/CardRegister.js
@@ -8,20 +8,23 @@ export default () => <div>
     <div className="homeRoot">
         <div className="RegisterImage" />
         <div className="homeBox">
-            <div className="mainTitle">
-                <div className="TextOne">
-                    <h1> <span className="textSubOne">Start</span> Tasking.</h1>
+            <div className="subBox">
+                <div className="mainTitle">
+                    <div className="TextOne">
+                        <h1> <span className="textSubOne">Start</span> Tasking.</h1>
+                    </div>
+                    <div className="TextTwo">
+                        <h2> Earn <span className="TextSubTwo">money</span> your way.</h2>
+                    </div>
+                    <div className="Textthree">
+                        <h3> Be someone's hero today. Earn money by helping <span className="textSubThree">people</span> with their everyday to-dos.</h3>
+                    </div>
                 </div>
-                <div className="TextTwo">
-                    <h2> Earn <span className="TextSubTwo">money</span>your way.</h2>
-                </div>
-                <div className="Textthree">
-                    <h3> Be someone's hero today. Earn money by helping <span className="textSubThree">people</span> with their everyday to-dos.</h3>
-                </div>
+                <ButtonRegister />
             </div>
         </div>
 
-        <ButtonRegister />
+
 
     </div>
 </div>
