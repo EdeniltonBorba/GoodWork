cced5ef520e9fb0409b9ad96b22e25756fcd18ce
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-07 10:15:34 +0100

    update thanks

--- src/components/CardThanks/index.js
+++ src/components/CardThanks/index.js
@@ -5,7 +5,9 @@ import "./img.css"
 
 export default () => <div className="familyRoot">
     <img className="familyImage" src={Background} />
-    <div className="familyText"> Thank you, it was a pleasure to have you here, soon we will return your request!</div>
+    <div className="thanksBox">
+        <div className="familyText"> Thank you, it was a pleasure to have you here, soon we will return your request!</div>
+    </div>
 </div>
 
 
