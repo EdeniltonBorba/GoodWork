c5af255f533153351b3890e9ef154676d3f901ee
Edenilton <edeniltonborba.adm@gmail.com>
2020-04-29 19:21:17 +0200

    update

--- src/components/BookAppointments/BookAppointments.js
+++ src/components/BookAppointments/BookAppointments.js
@@ -3,15 +3,10 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 import CardAppointment from '../BookAppointments/CardAppointmen.js'
 
-const ButtonApoi = {
-    backgroundColor: "#3f51b5",
-    color: "white",
-}
 
 export default function ResponsiveDialog() {
     const [open, setOpen] = React.useState(false);
