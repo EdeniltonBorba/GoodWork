c6ebbb254170b5ee5253c0f3fb41840d4e76f334
Edenilton <edeniltonborba.adm@gmail.com>
2020-04-30 19:56:30 +0200

    create new page welcome

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -4,15 +4,25 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { LogoOpening } from '../../components/Logo/Logo';
 import "./styles.css";
+import { Container } from "../../components/Grid";
 
 function Welcome() {
     return (
         <React.Fragment>
-            <AppBar position="relative">
+            <AppBar position="relative" className="navBar">
                 <Toolbar>
                     <LogoOpening />
                 </Toolbar>
             </AppBar>
+            <main>
+                <div className="">
+                    <Container maxWidth="sm">
+                        <Typography>
+
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
         </React.Fragment>
     )
 }
