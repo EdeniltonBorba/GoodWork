3a1d443138028d1c306dffbaa017270a48c07b7b
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-09 12:51:45 +0200

    add styleComponents

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import { LogoOpening } from '../../components/Logo/Logo';
 import { AppBar } from "./styles";
 import { Container } from "../../components/Grid";
+import { Link } from "react-router-dom";
 
 function Welcome() {
     return (
@@ -17,7 +18,21 @@ function Welcome() {
                 <div className="">
                     <Container maxWidth="sm">
                         <Typography>
+                            <div className="homeRoot">
+                                <div className="homeImage" />
+                                <div className="homeBox">
+                                    <div className="homeText"><span class="customHelp">Help</span> around the home, at your fingertips</div>
+                                </div>
+                            </div>
+                            <div className="homeSubtitle">
+                                <div className="mainTitle">
+                                    You don't always have to DIY. Get help from thousands of trusted Taskers for everything from home repairs to cleaning.
+        </div>
+                                <div className="homeCtas">
+                                    <Link to="/SignIn"><button className="buttonSign">Sign In</button></Link><Link to="/SignUp"> <button className="buttonSign">Sign Up</button></Link>
+                                </div>
 
+                            </div>
                         </Typography>
                     </Container>
                 </div>
