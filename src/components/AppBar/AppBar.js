6f24b4ac331afdac4bf31e1111cc5c1c0ea75996
Edenilton <edeniltonborba.adm@gmail.com>
2020-05-15 22:23:12 +0200

    add logo

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import { AppBar } from '../AppBar/styles.js';
-
+import LogoGoodWork from '../../components/Logo/index.js';
 
 function AppBarHome() {
     return (
         <React.Fragment>
             <AppBar>
                 <Toolbar>
-
+                    <LogoGoodWork />
                 </Toolbar>
             </AppBar>
         </React.Fragment >
